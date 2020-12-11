b51d5a4d934791a7360cc2cab0769f0d9933687c
amdoku <aqpoba@gmail.com>
2020-12-11 22:08:41 +0100

    [Day11] Part 2

--- Day11/main.cpp
+++ Day11/main.cpp
@@ -1,4 +1,3 @@
-#include <deque>
 #include <string_view>
 #include <fstream>
 #include <iterator>
@@ -7,9 +6,13 @@
 #include <algorithm>
 #include <cassert>
 #include <numeric>
+#include <complex>
 
-using Layout = std::deque<std::string>;
-using Change = std::pair<size_t, size_t>;
+using Layout = std::vector<std::string>;
+using Change = std::complex<size_t>;
+using Direction = std::complex<size_t>;
+using Coordinates = std::complex<size_t>;
+using SeatTestFunction = std::function<bool(Layout const&, size_t const, size_t const)>;
 
 constexpr auto FLOOR = '.';
 constexpr auto SEAT = 'L';
@@ -33,7 +36,7 @@ auto readFromFile(std::string_view filename) {
 
 	std::istream_iterator<T> start{istream};
 	std::istream_iterator<T> end{};
-	std::deque<T> data{start, end};
+	std::vector<T> data{start, end};
 	istream.close();
 	return data;
 }
@@ -61,10 +64,42 @@ constexpr auto countSurrounding = [](Layout const & layout, size_t const row, si
 	return counted >= AMOUNT;
 };
 
-constexpr auto willChangeToPerson = countSurrounding<SEAT, 8, true>;
-constexpr auto willChangeToSeat = countSurrounding<PERSON, 4, false>;
+template<char SEAT, int AMOUNT, bool COUNT_FLOOR>
+constexpr auto countVisible = [](Layout const & layout, size_t const row, size_t const coloumn) {
+	std::array<Direction, 8> constexpr directionVectors{{
+			{-1ul, -1ul}, {-1ul, 0}, {-1ul, +1},
+			{ 0, -1ul}, /*seat*/ { 0, +1},
+			{+1, -1ul}, {+1, 0}, {+1, +1}
+	}};
+	Coordinates origin{row, coloumn};
 
-bool step(Layout & layout) {
+	auto seen{0ul};
+
+	for(auto const & entry : directionVectors) {
+		Coordinates current{origin + entry};
+
+		while (   current.real() < layout.size()
+				&& current.imag() < layout.at(current.real()).length()) {
+			if (layout.at(current.real()).at(current.imag()) != FLOOR) {
+				if (layout.at(current.real()).at(current.imag()) == SEAT) {
+					seen++;
+				}
+				break;
+			}
+			current+=entry;
+		}
+		if (COUNT_FLOOR && !(current.real() < layout.size()
+				&& current.imag() < layout.at(current.real()).length())) {
+			seen++;
+		}
+	}
+
+	return seen >= AMOUNT;
+};
+
+
+
+bool step(Layout & layout, SeatTestFunction const & willChangeToPerson, SeatTestFunction const & willChangeToSeat) {
 	std::vector<Change> markedChanges;
 
 	auto rows{0ul};
@@ -84,7 +119,7 @@ bool step(Layout & layout) {
 	}
 
 	std::for_each(markedChanges.cbegin(), markedChanges.cend(), [&layout](auto const & pair) {
-		auto & seat = layout[pair.first][pair.second];
+		auto & seat = layout[pair.real()][pair.imag()];
 		if (seat == SEAT) {
 			seat = PERSON;
 		} else if (seat == PERSON) {
@@ -114,18 +149,40 @@ int main(int argc, char** argv) {
 	auto input{readFromFile<std::string>(argv[1])};
 
 	log("input size=") << input.size() << '\n';
+	{
+		bool progress{true};
+		int iteration{0u};
+		Layout part1{input};
 
-	bool progress{true};
-	int iteration{0u};
-	for(; progress && iteration < 1000; ++iteration) {
-		progress = step(input);
+		constexpr auto willChangeToPerson = countSurrounding<SEAT, 8, true>;
+		constexpr auto willChangeToSeat = countSurrounding<PERSON, 4, false>;
+		for (; progress && iteration < 1000; ++iteration) {
+			progress = step(part1, willChangeToPerson, willChangeToSeat);
+		}
+
+		log("Iteration=") << iteration << '\n';
+
+		log("Part 1: ")
+				<< std::accumulate(part1.cbegin(), part1.cend(), 0ul, [](size_t const &acc, std::string const &line) {
+					return acc + static_cast<unsigned long>(std::count(line.begin(), line.end(), PERSON));
+				}) << '\n';
 	}
 
-	log("Iteration=") << iteration << '\n';
+	{
+		Layout part2{input};
+		bool progress{true};
+		auto iteration{0ul};
+		constexpr auto willChangeToPerson = countVisible<SEAT, 8, true>;
+		constexpr auto willChangeToSeat = countVisible<PERSON, 5, false>;
+		for (; progress && iteration < 1000; ++iteration) {
+			progress = step(part2, willChangeToPerson, willChangeToSeat);
+		}
 
-	log("Part 1: ") << std::accumulate(input.cbegin(), input.cend(), 0ul, [](size_t const & acc, std::string const & line) {
-		return acc + static_cast<unsigned long>(std::count(line.begin(), line.end(), PERSON));
-	});
+		log("Part 2: ")
+				<< std::accumulate(part2.cbegin(), part2.cend(), 0ul, [](size_t const &acc, std::string const &line) {
+					return acc + static_cast<std::size_t>(std::count(line.begin(), line.end(), PERSON));
+				}) << '\n';
+	}
 
 	return 0;
 }
